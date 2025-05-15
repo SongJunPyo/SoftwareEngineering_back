f6240a33852bc2016b677c02ba88f84b636b018e
SongJunpyo <thdwnsvy123@naver.com>
2025-05-16 00:30:47 +0900

    DB연동

--- backend/database/base.py
+++ backend/database/base.py
@@ -4,16 +4,21 @@ from dotenv import load_dotenv
 import os
 
 # .env 로드
+<<<<<<< Updated upstream
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
+=======
+# load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
+# load_dotenv()
+# 현재 경로 기준으로 상위 두 디렉토리 위에 있는 .env를 명시적으로 로드
+env_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), '.env')
+load_dotenv(dotenv_path=env_path)
+>>>>>>> Stashed changes
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://users:ibdp@10.100.54.176:5432/software"
 
 if SQLALCHEMY_DATABASE_URL is None:
     raise ValueError("DATABASE_URL is not set in .env")
 
-# DB 엔진 및 세션 설정
-engine = create_engine(SQLALCHEMY_DATABASE_URL, future=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
-Base = declarative_base()
 
 # 의존성 주입용 세션
 def get_db():
@@ -22,6 +27,16 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# DB 엔진 및 세션 설정
+engine = create_engine(SQLALCHEMY_DATABASE_URL, future=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
+Base = declarative_base()
+
+print("DATABASE_URL =", repr(SQLALCHEMY_DATABASE_URL))
+print("CURRENT DIR:", os.getcwd())
+print(".env DATABASE_URL:", os.getenv("DATABASE_URL"))
+
 
 # DB 연결 테스트
 def check_db_connection():
@@ -47,4 +62,10 @@ def check_db_connection():
         raise
 
 if __name__ == "__main__":
+<<<<<<< Updated upstream
     check_db_connection()
+=======
+    check_db_connection()
+    print("BASE DIR:", os.path.dirname(os.path.dirname(__file__)))
+    print("DATABASE_URL =", SQLALCHEMY_DATABASE_URL)
+>>>>>>> Stashed changes
