e318d1fed9f2830a08a0f71513971f0100fbfc6e
SongJunpyo <thdwnsvy123@naver.com>
2025-05-20 14:34:39 +0900

    Update models

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,5 +1,7 @@
 from .user import User
 from .task import Task
-from .project import Project
-from. project import ProjectMember
+from .project import Project, ProjectMember
 from .workspace import Workspace
+from .comment_file import Comment, File
+from .logs_notification import Logs, Notification
+from .tag import Tag
