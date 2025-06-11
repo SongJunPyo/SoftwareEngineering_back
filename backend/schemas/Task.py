8cc404394fd8434ec6a3865345653ce339820d4b
SongJunPyo <133749359+SongJunPyo@users.noreply.github.com>
2025-06-11 23:34:23 +0900

    Update Task.py

--- backend/schemas/Task.py
+++ backend/schemas/Task.py
@@ -1,10 +1,43 @@
+# from pydantic import BaseModel
+# from typing import Optional
+# from datetime import datetime
+
+# class TaskCreateRequest(BaseModel):
+#     title: str
+#     assignee_id: Optional[int] = None  # 담당자 ID, 선택적
+#     parent_task_id: Optional[int] = None
+#     priority: str               # "low"|"medium"|"high"
+#     start_date: datetime
+#     due_date: datetime
+#     project_id: int
+
+#     class Config:
+#         orm_mode = True
+
+# class TaskResponse(BaseModel):
+#     task_id: int
+#     project_id: int
+#     parent_task_id: Optional[int]
+#     title: str
+#     assignee_id: Optional[int] = None  # 담당자 ID, 선택적
+#     priority: str
+#     start_date: datetime
+#     due_date: datetime
+#     status: str
+#     updated_at: Optional[datetime]
+#     description: Optional[str] = None  # 업무 설명
+#     assignee_name: Optional[str] = None  # 담당자 이름
+#     class Config:
+#         orm_mode = True
+
+
 from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 
 class TaskCreateRequest(BaseModel):
     title: str
-    assignee_id: Optional[int] = None  # 담당자 ID, 선택적
+    assignee_id: int
     parent_task_id: Optional[int] = None
     priority: str               # "low"|"medium"|"high"
     start_date: datetime
@@ -19,7 +52,7 @@ class TaskResponse(BaseModel):
     project_id: int
     parent_task_id: Optional[int]
     title: str
-    assignee_id: Optional[int] = None  # 담당자 ID, 선택적
+    assignee_id: int
     priority: str
     start_date: datetime
     due_date: datetime
